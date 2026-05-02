ade0ab8ca31d6a35786273cb0520921eb3d6ba5a
mahzabin-sayeka <mahzabinsayeka000212@gmail.com>
2026-05-03 01:00:17 +0600

    animation problem solved in marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,16 +3,22 @@ const Marquee = () => {
 
   return (
     <div className="bg-black py-3 md:py-4 overflow-hidden border-y border-gray-800 w-full flex">
+      {/* animation */}
+      <style>{`
+        @keyframes marquee {
+          0% { transform: translateX(0); }
+          100% { transform: translateX(-100%); }
+        }
+      `}</style>
       
       <div className="flex whitespace-nowrap">
-        
-     {/* First ,, */}
+       
         <div className="flex shrink-0 items-center text-white uppercase tracking-[0.15em] md:tracking-[0.2em] text-xs md:text-sm animate-[marquee_30s_linear_infinite]">
           <span className="px-4">{marqueeText}</span>
           <span className="px-4">{marqueeText}</span>
         </div>
 
- {/* loop.. */}
+       
         <div className="flex shrink-0 items-center text-white uppercase tracking-[0.15em] md:tracking-[0.2em] text-xs md:text-sm animate-[marquee_30s_linear_infinite]">
           <span className="px-4">{marqueeText}</span>
           <span className="px-4">{marqueeText}</span>
