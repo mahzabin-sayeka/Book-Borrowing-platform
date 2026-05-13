1029b7995f543facb8f002fff3d06d6ad779e929
mahzabin-sayeka <mahzabinsayeka000212@gmail.com>
2026-05-13 19:57:13 +0600

    error fix try

--- src/app/all-books/[id]/page.jsx
+++ src/app/all-books/[id]/page.jsx
@@ -1,4 +1,5 @@
-import booksData from "@/data/books.json";
+// import booksData from "@/data/books.json";
+import booksData from "../../../data/books.json";
 import Link from "next/link";
 
 export default async function BookDetailsPage({ params }) {
