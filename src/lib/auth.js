8e339cf47a00544594430fb09454666afd52dd6b
mahzabin-sayeka <mahzabinsayeka000212@gmail.com>
2026-05-13 22:17:05 +0600

    error fixing

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,18 @@
+// import { betterAuth } from "better-auth";
+// import { prismaAdapter } from "better-auth/adapters/prisma";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export const auth = betterAuth({
+//     database: prismaAdapter(prisma, {
+//         provider: "mongodb",
+//     }),
+//     emailAndPassword: {
+//         enabled: true,
+//     },
+// });
+
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
@@ -11,4 +26,9 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    // new
+    trustedOrigins: ["https://book-borrowing-platform-five.vercel.app"],
+    advanced: {
+        trustProxy: true
+    }
 });
